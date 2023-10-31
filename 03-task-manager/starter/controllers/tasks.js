bfc1582c139d9bd4937ba1215a2ef7cc55b91943
fullstack-dan <aouloudan@icloud.com>
2023-10-31 17:03:55 -0400

    code done

---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,24 @@
+const Task = require("../models/TaskModel.js");
+
+const getAllTasks = (req, res) => {
+  res.send("all items");
+};
+
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+};
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
